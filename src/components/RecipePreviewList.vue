d5915184e2bc1dd5f9121dad3cc0a8be78454448
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2020-07-13 15:59:34 +0300

    added my favoritez, my family recipes and my recipes

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -48,7 +48,50 @@ export default {
       } catch (error) {
         console.log(error);
       }
+    },
+    async myFavoriteRecipes() {
+      try {
+        const response = await this.axios.get(
+          "https://test-for-3-2.herokuapp.com/recipes/random"
+        );
+        // console.log(response);
+        const recipes = response.data.recipes;
+        this.recipes = [];
+        this.recipes.push(...recipes);
+        console.log(this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async myRecipes() {
+      try {
+        const response = await this.axios.get(
+          "https://test-for-3-2.herokuapp.com/recipes/random"
+        );
+        // console.log(response);
+        const recipes = response.data.recipes;
+        this.recipes = [];
+        this.recipes.push(...recipes);
+        console.log(this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async myFamilyRecipes() {
+      try {
+        const response = await this.axios.get(
+          "https://test-for-3-2.herokuapp.com/recipes/random"
+        );
+        // console.log(response);
+        const recipes = response.data.recipes;
+        this.recipes = [];
+        this.recipes.push(...recipes);
+        console.log(this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
     }
+
   }
 };
 </script>
