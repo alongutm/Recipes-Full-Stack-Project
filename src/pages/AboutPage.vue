b773100f01e54875c3c6805e949a94bb99564dfb
Alon <51872854+alongutm@users.noreply.github.com>
2020-07-14 20:12:11 +0300

    new branch

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+      <h1>
+          About Us
+      </h1>
+      <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4500"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="font-size:300%; text-shadow: 2px 2px 3px black; background-color:white;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=59"
+        text="We are Amit & Alon and this is our recipes site :)"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+     <a href="https://sise-web-development-environments.github.io/203169693/" target="_blank">
+      <b-carousel-slide
+        img-src="https://res.cloudinary.com/dvc9uejg6/image/upload/v1594582885/site2_kwonf6.jpg">
+        <p style="color:black;" >Alon's Personal Site</p>
+      </b-carousel-slide>
+    </a>
+
+     <a href="https://amitshakarchy.github.io/313278889/" target="_blank">
+      <b-carousel-slide
+        img-src="https://res.cloudinary.com/dvc9uejg6/image/upload/v1594574852/htmlIMG_j51j0o.jpg">
+        <p style="color:black;" >Amit's Personal Site</p>
+      </b-carousel-slide>
+    </a>
+
+    <a href="https://sise-web-development-environments.github.io/assignment2-amit_alon/" target="_blank">
+      <b-carousel-slide
+      
+        img-src="https://res.cloudinary.com/dvc9uejg6/image/upload/v1594580911/pacman1_fpvcbh.jpg">
+        <p style="color:yellow;" >Pacman Project</p>
+      </b-carousel-slide>
+    </a>
+    
+     
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+h1 { color: #4188f3; 
+font-family: 'Rouge Script', cursive; 
+font-size: 110px; 
+font-weight: normal; 
+line-height: 108px; 
+margin: 0 0 0px; 
+text-align: center; 
+text-shadow: 3px 3px 5px #000000;
+background-color: rgb(205, 233, 226);
+}
+
+</style>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,5 +1,108 @@
 <template>
   <div class="container">
-    <h1 class="title">Search Page</h1>
+     <h1 class="title">Search Recipes</h1>
+<b-form @submit.prevent="search">
+      <b-form-input v-model="query" id="query" placeholder="Enter your query here" required></b-form-input>
+      <!-- <div class="mt-2">Value: {{ query }}</div> -->
+      <b>Number of results:</b>
+      <b-form-select v-model="selectedAmount" id="amount" :options="searchSizes" ></b-form-select>
+      <b>Cuisine:</b>
+      <b-form-select v-model="selectedCuisine" id="cuisine" :options="cuisines"></b-form-select>
+      <b>Diet:</b>
+      <b-form-select v-model="selectedDiet" id="diet" :options="diets"  ></b-form-select>
+      <b>Intolerance:</b>
+      <b-form-select v-model="selectedIntolerance" id="intolerance" :options="intolerances"></b-form-select>
+      <b-button :disabled="query.length == 0" size="lg" type="submit" block variant="primary">search</b-button>
+      <!-- <b-form-group label="Sort the results:" v-if="responsed_recipes.length != 0">
+        <b-form-radio-group name="radio-button" v-model="selected_sorts" :options="sorts" switches></b-form-radio-group>
+        <b-button variant="secondary" @click="sortHandle">Sort!</b-button>
+      </b-form-group> -->
+    </b-form>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data () {
+    return{
+    query: '',
+    selectedAmount: 5,
+    selectedCuisine: null,
+    selectedIntolerance: null,
+    selectedDiet: null,
+    cuisines: [
+        'African',
+        'American',
+        'British',
+        'Cajun',
+        'Caribbean',
+        'Chinese',
+        'Eastern European',
+        'European',
+        'French',
+        'German',
+        'Greek',
+        'Indian',
+        'Irish',
+        'Italian',
+        'Japanese',
+        'Jewish',
+        'Chinese',
+        'Korean',
+        'Latin American',
+        'Mediterranean',
+        'Mexican' ,
+        'Middle Eastern',
+        'Nordic',
+        'Southern',
+        'Spanish',
+        'Thai',
+        'Vietnamese'
+      ],
+    intolerances: [
+        'Dairy' ,
+        'Egg',
+        'Gluten',
+        'Grain' ,
+        'Peanut' ,
+        'Seafood' ,
+        'Sesame' ,
+        'Shellfish',
+        'Soy' ,
+        'Sulfite',
+        'Tree Nut',
+        'Wheat' 
+      ],
+    diets: [
+        'Gluten Free' ,
+        'Ketogenic',
+        'Vegetarian' ,
+        'Lacto-Vegetaria',
+        'Ovo-Vegetarian',
+        'Vegan',
+        'Pescetarian',
+        'Paleo',
+        'Primal',
+        'Whole30'
+      ],
+      searchSizes: [5,10,15]
+    }
+    
+  },
+  methods:
+  {
+    
+    search(){
+      console.log("function search!!!");
+    }
+    
+  }
+}
+</script>
+
+<style  src="vue-multiselect/dist/vue-multiselect.min.css">
+
+</style>
