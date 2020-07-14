9e91f0e3c5eea6757bf79bd1c33fb3049a46d1c7
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2020-07-14 22:37:14 +0300

    Update NotFoundPage.vue

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="container">
-    <h1>Four Oh Four you didn't</h1>
-    <router-link to="/" exact>ET Go Home</router-link>
+  <div class="center">
+    <h4>What are you looking for, honey?</h4>
+    <b-card
+      img-src="https://img.icons8.com/windows/96/000000/nothing-found.png"
+      img-alt="Card image"
+      img-right
+      class="mb-3"
+    >
+      <b-card-text>
+        It seems like you went lost. Let me help you to find your way
+        <router-link :to="{ name: 'main' }">home</router-link>!
+      </b-card-text>
+    </b-card>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10%;
+  width: 50%;
+}
+</style>
