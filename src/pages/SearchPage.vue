13376eac802401739dea3171006c84d43791c9af
Alon <51872854+alongutm@users.noreply.github.com>
2020-07-15 10:20:25 +0300

    search in pregress

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="container">
      <h1 class="title">Search Recipes</h1>
-<b-form @submit.prevent="search">
+    <b-form v-if="!searched" @submit.prevent="search">
       <b-form-input v-model="query" id="query" placeholder="Enter your query here" required></b-form-input>
       <!-- <div class="mt-2">Value: {{ query }}</div> -->
       <b>Number of results:</b>
       <b-form-select v-model="selectedAmount" id="amount" :options="searchSizes" ></b-form-select>
       <b>Cuisine:</b>
       <b-form-select v-model="selectedCuisine" id="cuisine" :options="cuisines"></b-form-select>
+      <div class="mt-2">Value: {{ selectedCuisine }}</div>
       <b>Diet:</b>
       <b-form-select v-model="selectedDiet" id="diet" :options="diets"  ></b-form-select>
       <b>Intolerance:</b>
@@ -18,10 +19,18 @@
         <b-button variant="secondary" @click="sortHandle">Sort!</b-button>
       </b-form-group> -->
     </b-form>
+    <RecipePreviewList
+          v-else
+          ref="RecipePreviewList"
+          title="Explore These Recipes"
+          class="RandomRecipes center"
+        />
   </div>
 </template>
 
 <script>
+import RecipePreviewList from "../components/RecipePreviewList";
+
 export default {
   components: {
 
@@ -29,6 +38,7 @@ export default {
   data () {
     return{
     query: '',
+    searched: false,
     selectedAmount: 5,
     selectedCuisine: null,
     selectedIntolerance: null,
@@ -95,8 +105,42 @@ export default {
   methods:
   {
     
-    search(){
-      console.log("function search!!!");
+    async search(){
+      this.searched = true;
+      console.log("functionn search!");
+      try {
+        let cuisineString = "";
+        if(this.selectedCuisine != null){
+          console.log("");
+          cuisineString = "?cuisine=" + this.selectedCuisine;
+
+        }
+        console.log("cuisine string is " + cuisineString);
+        console.log("########################## #########");
+        console.log(`/search/query/${this.query}/amount/${this.selectedAmount}` + cuisineString);
+        let endPoint = "recipes/";
+        if(this.$root.store.username){
+          endPoint ="profiles/";
+        }
+        const response = 
+        await this.axios.get(`http://localhost:3000/${endPoint}/search/query/${this.query}/amount/${this.selectedAmount}` , 
+        {
+          params: {
+            // query: this.query, 
+            // number: this.selectedAmount,
+            ...(this.selectedCuisine ? { cuisine: this.selectedCuisine } : {}),
+            ...(this.selectedIntolerance ? { intolerances: this.selectedIntolerance } : {}),
+            ...(this.selectedDiet ? { diet: this.selectedDiet } : {})
+          }
+        });
+      //show recipes with component
+
+      }
+
+        catch (error) {
+        console.log(error);
+      }
+
     }
     
   }
